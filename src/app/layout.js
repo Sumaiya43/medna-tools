99141563ca0ab3c0f2123b66e0ee81dc173cd773
Mazeda Begum <sumaiyabegum43@gmail.com>
2026-02-27 01:28:32 -0800

    speed insight added

--- src/app/layout.js
+++ src/app/layout.js
@@ -1,4 +1,5 @@
 import { Geist, Geist_Mono } from "next/font/google";
+import { SpeedInsights } from "@vercel/speed-insights/next";
 import "./globals.css";
 
 const geistSans = Geist({
@@ -22,6 +23,7 @@ export default function RootLayout({ children }) {
     <html lang="en">
       <body className={`${geistSans.variable} ${geistMono.variable}`}>
         {children}
+        <SpeedInsights />
       </body>
     </html>
   );
